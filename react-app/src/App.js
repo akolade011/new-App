cae5ebaf6ea8c0888d35e28c96538933d7ec6a23
Hassan Akolade <akoladeakinwande@hotmail.com>
2022-01-28 17:21:25 +0000

    1st function created

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import Expenseitems from './components/Expenseitems.js';
+const getName = prompt('Enter your name: ');
+const getMpvie = prompt('Enter movie name: ');
+const person2 = new Expenseitems(getName);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Your name is:{person2}</h1>
+      <h1>Your movie name is:{getMpvie}</h1>
+      {console.log(person2)}
     </div>
   );
 }
---
+++ react-app/src/components/Expenseitems.js
@@ -0,0 +1,12 @@
+import './Expenseitems.scss';
+
+function Expenseitems(input) {
+  return (
+    <div>
+      {input}
+      {}
+    </div>
+  );
+}
+
+export default Expenseitems;
