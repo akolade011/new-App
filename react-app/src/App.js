32db4320087a1ab604fb21415f848104a2ea40a2
Hassan Akolade <akoladeakinwande@hotmail.com>
2022-01-28 23:39:20 +0000

    test code created

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.scss';
-import Expenseitems from './components/Expenseitems.js';
-const getName = prompt('Enter your name: ');
-const getMpvie = prompt('Enter movie name: ');
-const person2 = new Expenseitems(getName);
+import ExpenseData from './components/Expenseitems.js';
+// const getName = prompt('Enter your name: ');
+// const getMpvie = prompt('Enter movie name: ');
+
 function App() {
+  let Data1 = new ExpenseData();
   return (
-    <div className="App">
-      <h1>Your name is:{person2}</h1>
-      <h1>Your movie name is:{getMpvie}</h1>
-      {console.log(person2)}
+    <div>
+      {}
+      {Data1}
     </div>
   );
 }
