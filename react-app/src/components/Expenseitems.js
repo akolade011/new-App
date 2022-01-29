11c468f7a15c58795b865b7b49adf2c5b0228674
Hassan Akolade <akoladeakinwande@hotmail.com>
2022-01-29 13:53:23 +0000

    no message

--- react-app/src/components/Expenseitems.js
+++ react-app/src/components/Expenseitems.js
@@ -1,12 +1,22 @@
 import './Expenseitems.scss';
 
-function Expenseitems(input) {
-  return (
-    <div>
-      {input}
-      {}
-    </div>
-  );
+function Expenseitems(x) {
+  const food = 'rice';
+  const price = 20.0;
+  const description = 'best food ever for the price of £20.00';
+  function Expenseitems(input) {
+    return (
+      <div className="expense-item">
+        <div className="expense-item__description">
+          <h2>{food}</h2>
+        </div>
+        <div className="expense-item__price">
+          <h2>{price}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Expenseitems;
