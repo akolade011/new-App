54409d27e1bb023ad4b92b798bd8e8ab33df9d70
Hassan Akolade <akoladeakinwande@hotmail.com>
2022-01-29 00:53:26 +0000

    Revert "test code created"
    This reverts commit 32db4320087a1ab604fb21415f848104a2ea40a2.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.scss';
-import ExpenseData from './components/Expenseitems.js';
-// const getName = prompt('Enter your name: ');
-// const getMpvie = prompt('Enter movie name: ');
-
+import Expenseitems from './components/Expenseitems.js';
+const getName = prompt('Enter your name: ');
+const getMpvie = prompt('Enter movie name: ');
+const person2 = new Expenseitems(getName);
 function App() {
-  let Data1 = new ExpenseData();
   return (
-    <div>
-      {}
-      {Data1}
+    <div className="App">
+      <h1>Your name is:{person2}</h1>
+      <h1>Your movie name is:{getMpvie}</h1>
+      {console.log(person2)}
     </div>
   );
 }
