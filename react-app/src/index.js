202fcc5e76e07bb9f60e27717f541207e928e568
Hassan Akolade <akoladeakinwande@hotmail.com>
2022-06-02 20:09:16 +0100

    more deleted files

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/App.scss';
-import App from './styles/App.css';
+import ReactDOM from "react-dom";
+import "./styles/index.scss";
+import React, { Component } from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
