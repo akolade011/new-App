492495847954b11fd54697ff5e8d5f616f087d5b
Hassan Akolade <akoladeakinwande@hotmail.com>
2022-06-02 20:47:50 +0100

    bootstrap installed

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class App extends Component {
   render() {
